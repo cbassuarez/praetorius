4c57a09e427b16cf272908ff8319fb3c089688a2
Stage Devices <developer@stagedevices.com>
2025-11-12 21:21:20 -0800

    chore: index.ts routing

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -2,14 +2,14 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
 // Global components available in markdown
-import PraePlayground from '../../components/PraePlayground.vue'
-import GitHubMeta from '../../components/GitHubMeta.vue'
+import PraePlayground from '../components/PraePlayground.vue'
+import GitHubMeta from '../components/GitHubMeta.vue'
 import NpmBadge from '../components/NpmBadge.vue'
-import GitHubChangelog from '../../components/GitHubChangelog.vue'
-import CLIBlock from '../../components/CLIBlock.vue'
-import SchemaViewer from '../../components/SchemaViewer.vue'
-import ErrorGlossary from '../../components/ErrorGlossary.vue'
-import SinceBadge from '../../components/SinceBadge.vue'
+import GitHubChangelog from '../components/GitHubChangelog.vue'
+import CLIBlock from '../components/CLIBlock.vue'
+import SchemaViewer from '../components/SchemaViewer.vue'
+import ErrorGlossary from '../components/ErrorGlossary.vue'
+import SinceBadge from '../components/SinceBadge.vue'
 
 export default {
   ...DefaultTheme,
