2f3079a6d4ff2b40c86d345bc5a25b407aabd2b2
Stage Devices <developer@stagedevices.com>
2025-11-12 22:12:14 -0800

    fix: npmpadge.vue

--- website/docs/components/npmbadge.vue
+++ website/docs/components/npmbadge.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-const props = defineProps<{ pkg: string }>()
+import { onMounted, ref } from \"vue\"
 const version = ref<string|null>(null)
-onMounted(async () => {
-  try { const r = await fetch(`https://registry.npmjs.org/${props.pkg}/latest`); const j = await r.json(); version.value = j.version ?? null } catch{}
-})
+onMounted(async()=>{try{const r=await fetch(\"https://registry.npmjs.org/praetorius/latest\");const j=await r.json();version.value=j.version||null}catch{}})
 </script>
-
-<template>
-  <span>npm <b v-if="version">v{{ version }}</b></span>
-</template>
+<template><span>npm <b v-if=\"version\">v{{ version }}</b></span></template>
